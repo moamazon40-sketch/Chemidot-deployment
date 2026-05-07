ee356e55cdffa4a9db92ea8761553a04df0da758
moamazon40-sketch <moamazon40@gmail.com>
2026-05-08 00:13:52 +0300

    Load API bundle dynamically

--- api/[...path].js
+++ api/[...path].js
@@ -1,3 +1,8 @@
-import app from "../artifacts/api-server/dist/app.mjs";
+let appPromise;
 
-export default app;
+export default async function handler(req, res) {
+  appPromise ??= import("../artifacts/api-server/dist/app.mjs");
+  const { default: app } = await appPromise;
+
+  return app(req, res);
+}
